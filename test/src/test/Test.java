8ff574ee30a7897427ccc0912807ab7c87b9aa73
Jagdeep Kaur <jagdeepk0908@gmail.com>
2023-06-18 11:08:04 -0400

    Changed Test.java

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -5,8 +5,9 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		
 		System.out.println("Hello Sunday");
-
+		System.out.println("Its a beautiful day");
 	}
 
 }
